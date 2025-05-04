4ea66a88af4573dd210e75db23d3f9e20e35aab4
Alejandro <alejandroandres.pepito@gmail.com>
2025-05-04 04:27:50 -0400

    Revised to allow the user to enter the number of gallons of gasoline and the number of miles driven

--- Chapter2Exercise10.cpp
+++ Chapter2Exercise10.cpp
@@ -2,7 +2,7 @@
 
     File name: Chapter2Exercise10.cpp
     Programmer/Developer: Alejandro Andres Pepito
-    Date: 2/20/2025
+    Date: 2/20/2025 (Revised on 5/04/2025)
     Requirements summary:
         "Write a program that calculates the number of miles per gallon of gas a car gets."
 
@@ -14,15 +14,20 @@ using namespace std;
 
 int main()
 {
-   // Hardcoded values for given problem
-   double gallons = 15.0;
-   double miles = 375.0;
+    double gallons, miles;
 
-   // Calculate miles per gallon
-   double mpg = miles / gallons;
+    // Get input from the user
+    cout << "Enter the number of gallons of gasoline: ";
+    cin >> gallons;
 
-   // Output result
-   cout << "A car holds 15 gallons of gasoline and can travel 375 miles before refueling.\nUsing the formula \"MPG = Miles Driven / Gallons of Gas Used\", we can determine that the car gets " << mpg << " miles per gallon." << endl;
+    cout << "Enter the number of miles driven: ";
+    cin >> miles;
+
+    // Calculate miles per gallons
+    double mpg = miles / gallons;
+
+    // Output result
+    cout << "The car gets " << mpg << " miles per gallon." << endl;
 
    return 0;
 }
